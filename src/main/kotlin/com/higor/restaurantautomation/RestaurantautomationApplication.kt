497cf533cf0bbf45a7967e87573501f3d0dc5ac6
Higor F. Oliveira <higoroliveiiira@gmail.com>
2021-01-19 20:36:05 -0300

    Iniciando o projeto springboot

---
+++ src/main/kotlin/com/higor/restaurantautomation/RestaurantautomationApplication.kt
@@ -0,0 +1,11 @@
+package com.higor.restaurantautomation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestaurantautomationApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestaurantautomationApplication>(*args)
+}
---
+++ src/test/kotlin/com/higor/restaurantautomation/RestaurantautomationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.higor.restaurantautomation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestaurantautomationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
