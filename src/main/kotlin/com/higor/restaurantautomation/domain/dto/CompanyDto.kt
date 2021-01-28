a927160a20574af0eb77eba49e4d70bbf2e5f019
Higor <higoroliveiiira@gmail.com>
2021-01-28 00:42:49 -0300

    -  Finished all endpoint implementation from Company Resource

--- src/main/kotlin/com/higor/restaurantautomation/domain/dto/CompanyDto.kt
+++ src/main/kotlin/com/higor/restaurantautomation/domain/dto/CompanyDto.kt
@@ -1,10 +1,23 @@
 package com.higor.restaurantautomation.domain.dto
 
 
-data class createCompanyDto(
+data class CreateCompanyDto(
         val name: String,
         val email: String,
         val password: String,
         val phone: String,
         val document: String
+)
+
+data class UpdateCompanyDto(
+        val id: Long,
+        val name: String? = null,
+        val email: String? = null,
+        val phone: String? = null,
+        val document: String? = null
+)
+
+data class UpdateCompanyPasswordDto(
+        val id: Long,
+        val password: String
 )
