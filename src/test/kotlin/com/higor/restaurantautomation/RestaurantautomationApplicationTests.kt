d5186c80d3ddf4bcd4e90fcb760048395d028d2e
Higor <higoroliveiiira@gmail.com>
2021-02-03 23:15:11 -0300

    Change context test for all tests

--- src/test/kotlin/com/higor/restaurantautomation/RestaurantautomationApplicationTests.kt
+++ src/test/kotlin/com/higor/restaurantautomation/RestaurantautomationApplicationTests.kt
@@ -2,8 +2,10 @@ package com.higor.restaurantautomation
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
 
 @SpringBootTest
+@ContextConfiguration(classes=[RestaurantautomationApplication::class])
 class RestaurantautomationApplicationTests {
 
 	@Test
