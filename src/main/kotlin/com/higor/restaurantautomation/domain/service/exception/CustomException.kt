e16a26a084f852d9b40ddcb8e4628971102a6e67
Higor F. Oliveira <higoroliveiiira@gmail.com>
2021-05-22 18:34:09 -0300

    feat: Change my CustomException inheritance

--- src/main/kotlin/com/higor/restaurantautomation/domain/service/exception/CustomException.kt
+++ src/main/kotlin/com/higor/restaurantautomation/domain/service/exception/CustomException.kt
@@ -2,6 +2,6 @@ package com.higor.restaurantautomation.domain.service.exception
 
 import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
 
-abstract class CustomException(message: String) : Exception(message) {
+abstract class CustomException(message: String) : RuntimeException(message) {
     open val statusCode = INTERNAL_SERVER_ERROR
 }
