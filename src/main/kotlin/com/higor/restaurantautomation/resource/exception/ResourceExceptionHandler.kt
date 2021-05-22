0fb0f49d90b6c37ad486998c6602de1da25f9d99
Higor F. Oliveira <higoroliveiiira@gmail.com>
2021-05-22 18:16:44 -0300

    feat: handling exception when requestBody come wrong

--- src/main/kotlin/com/higor/restaurantautomation/resource/exception/ResourceExceptionHandler.kt
+++ src/main/kotlin/com/higor/restaurantautomation/resource/exception/ResourceExceptionHandler.kt
@@ -4,16 +4,14 @@ import com.higor.restaurantautomation.domain.dto.StandardError
 import com.higor.restaurantautomation.domain.service.exception.CustomException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.ResponseStatus
-import java.io.IOException
 import java.time.Instant
 import javax.servlet.http.HttpServletRequest
 
 @ControllerAdvice
 class ResourceExceptionHandler {
-    private val messageList: HashSet<String> = HashSet()
 
     @ExceptionHandler(CustomException::class)
     fun customExceptionsHandle(ex: CustomException, request: HttpServletRequest): ResponseEntity<StandardError> =
@@ -21,15 +19,19 @@ class ResourceExceptionHandler {
             .status(ex.statusCode)
             .body(standardErrorFactory(ex.localizedMessage, request))
 
-    @ExceptionHandler(IOException::class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    fun ioException(ex: IOException, request: HttpServletRequest): ResponseEntity<StandardError> =
-        ResponseEntity
-            .status(HttpStatus.PRECONDITION_FAILED)
-            .body(standardErrorFactory(ex.localizedMessage, request))
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun methodArgumentException(
+        ex: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ResponseEntity<StandardError>  {
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(standardErrorFactory("Invalid Request Body", request))
+    }
 
     private fun standardErrorFactory(message: String, request: HttpServletRequest): StandardError {
-        this.messageList.add(message)
-        return StandardError(this.messageList, request.requestURI, Instant.now())
+        val messages = HashSet<String>()
+        messages.add(message)
+        return StandardError(messages, request.requestURI, Instant.now())
     }
 }
