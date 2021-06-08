816c79339f11cc112ec426b836cf8dca6f9d6663
Higor Freitas de Oliveira <higoroliveiiira@gmail.com>
2021-06-07 22:19:59 -0300

    Feature/pageable resources (#18)
    * feat: Make getAllProducts endpoint Pageable
    
    * feat: Make getAllBoards endpoint Pageable
    
    * feat: Make getAllBoards endpoint Pageable

--- src/main/kotlin/com/higor/restaurantautomation/resource/BoardResource.kt
+++ src/main/kotlin/com/higor/restaurantautomation/resource/BoardResource.kt
@@ -1,9 +1,11 @@
 package com.higor.restaurantautomation.resource
 
-import com.higor.restaurantautomation.domain.dto.CreateBoardDto
-import com.higor.restaurantautomation.domain.entity.Board
+import com.higor.restaurantautomation.domain.dto.BoardResponse
+import com.higor.restaurantautomation.domain.dto.CreateBoard
+import com.higor.restaurantautomation.domain.dto.PagedBoardsResponse
 import com.higor.restaurantautomation.domain.service.contracts.BoardServiceContract
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -18,20 +20,32 @@ import java.util.UUID
 
 @RestController
 @RequestMapping("/api")
-class BoardResource(@Autowired private val boardService: BoardServiceContract) {
+class BoardResource(
+    @Autowired private val boardService: BoardServiceContract
+) {
 
     @GetMapping("/board")
-    fun getAllBoards(@RequestAttribute("companyId") companyId: UUID): ResponseEntity<List<Board>> =
-        ResponseEntity.ok(this.boardService.getAll(companyId))
+    fun getAllBoards(
+        @RequestAttribute("companyId") companyId: UUID,
+        pageable: Pageable
+    ): ResponseEntity<PagedBoardsResponse> {
+        val pagedBoardsResponse = this.boardService.getAll(companyId, pageable)
+
+
+        return ResponseEntity.ok(pagedBoardsResponse)
+    }
 
     @GetMapping("/board/{id}")
-    fun getBoard(@PathVariable id: UUID): ResponseEntity<Board> = ResponseEntity.ok(this.boardService.getById(id))
+    fun getBoard(@PathVariable id: UUID): ResponseEntity<BoardResponse> {
+        val board = this.boardService.getById(id)
+        return ResponseEntity.ok(board.toBoardResponse())
+    }
 
     @PostMapping("/board")
     fun create(
-        @RequestBody createDto: CreateBoardDto,
+        @RequestBody createDto: CreateBoard,
         @RequestAttribute("companyId") companyId: UUID
-    ): ResponseEntity<Board> {
+    ): ResponseEntity<BoardResponse> {
         createDto.companyId = companyId
         val board = this.boardService.create(createDto)
 
@@ -41,7 +55,7 @@ class BoardResource(@Autowired private val boardService: BoardServiceContract) {
     }
 
     @DeleteMapping("/board/{id}")
-    fun delete(@PathVariable id: UUID): ResponseEntity<Board> {
+    fun delete(@PathVariable id: UUID): ResponseEntity<BoardResponse> {
         this.boardService.delete(id)
 
         return ResponseEntity.noContent().build()
