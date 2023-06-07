04498b8a5781137cde502c2b29f8323e837cc3db
Higor Oliveira <higoroliveiiira@gmail.com>
2023-06-06 23:57:49 -0300

    feat(api): logging exception messages

--- src/main/kotlin/com/higor/restaurantautomation/controller/exception/ExceptionHandler.kt
+++ src/main/kotlin/com/higor/restaurantautomation/controller/exception/ExceptionHandler.kt
@@ -5,6 +5,7 @@ import com.higor.restaurantautomation.domain.service.exception.ApiErrorCodes
 import com.higor.restaurantautomation.domain.service.exception.ApiException
 import com.higor.restaurantautomation.utils.extensions.from
 import jakarta.servlet.http.HttpServletRequest
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
@@ -15,11 +16,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 @ControllerAdvice
 class ExceptionHandler {
 
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     @ExceptionHandler(ApiException::class)
     fun handle(
         ex: ApiException,
         request: HttpServletRequest,
     ): ResponseEntity<StandardError> {
+        logExceptionMessage(ex.localizedMessage)
         return ResponseEntity
             .status(ex.status)
             .body(standardErrorFactory(ex.code, ex.details, ex.message!!))
@@ -30,6 +34,7 @@ class ExceptionHandler {
         ex: MethodArgumentNotValidException,
         request: HttpServletRequest,
     ): ResponseEntity<StandardError> {
+        logExceptionMessage(ex.localizedMessage)
         val errors = ex.bindingResult.fieldErrors.map { it.defaultMessage!! }.toSet()
         return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
@@ -41,9 +46,14 @@ class ExceptionHandler {
         ex: HttpMessageNotReadableException,
         request: HttpServletRequest,
     ): ResponseEntity<StandardError> {
+        logExceptionMessage(ex.localizedMessage)
         return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
             .body(StandardError.from(ApiErrorCodes.BAD_REQUEST))
+    }
+
+    private fun logExceptionMessage(message: String) {
+        logger.warn(message)
     }
 
     private fun standardErrorFactory(code: String, details: String?, message: String): StandardError {
